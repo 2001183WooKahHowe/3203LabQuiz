63953b2584e9a9e2346dd4ce03b935a52ebd37b3
Woo Kah Howe <2001183@sit.singaporetech.edu.sg>
2022-11-17 15:47:24 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_jwt_extended import (
-    JWTManager, jwt_required, get_jwt_identity, create_access_token, create_refresh_token, jwt_refresh_token_required, get_raw_jwt
-    )    
+#from flask_jwt_extended import (
+#    JWTManager, jwt_required, get_jwt_identity, create_access_token, create_refresh_token, jwt_refresh_token_required, get_raw_jwt
+#    )    
     
 #from wtforms import StringField, PasswordField, SubmitField
 app = Flask(__name__)
 
-app.config['JWT_SECRET_KEY'] = 'super-secret'
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+#app.config['JWT_SECRET_KEY'] = 'super-secret'
+#app.config['JWT_BLACKLIST_ENABLED'] = True
+#app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
-jwt= JWTManager(app)
-blacklist = set()
+#jwt= JWTManager(app)
+#blacklist = set()
 
 
 @app.route('/')
