717eafbda47a7f2f1bcdde37fbee70614f08a00e
Woo Kah Howe <2001183@sit.singaporetech.edu.sg>
2022-11-17 15:22:18 +0800

    min pw len 8

--- webapp/app.py
+++ webapp/app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, request
-from wtforms import StringField, PasswordField, SubmitField
+#from wtforms import StringField, PasswordField, SubmitField
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Flask Dockerized"
-
-@app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['password'] != 'admin':
             error = "invalid creds"
         else:
             return redirect(url_for('home'))        
     return render_template('login.html', error=error)
+
+"""
+
+Password at least 8 char
+ensur epw not in wordlist
+
+hash password
+
+"""
 
 
 if __name__ == '__main__':
